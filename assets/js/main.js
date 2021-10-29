3fc47616c9d12e1d1f233a10a64797142844f6e7
Musa Yazlık <33998073+musayazlik@users.noreply.github.com>
2021-10-29 11:26:23 +0300

    main.js oluşturuldu.
    -- main.js oluşturuldu.
    -- Tarih yönetimi için kodlama eklendi.

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+
+$(function () {
+   $('#tast-data').daterangepicker({
+      singleDatePicker: true,
+      showDropdowns: true,
+      minYear: 2021,
+      maxYear: 2030,
+      opens: "right",
+      drops: "auto",
+
+   });
+});
+
